fecd14f676a7e73435bce93a6aa239a40b62a03b
CHUYUN527-LXP\LiXiaoping <xpli126@126.com>
2017-10-08 12:00:51 +0800

    创建程序，安装模板

---
+++ grails-app/init/lms2017a/Application.groovy
@@ -0,0 +1,10 @@
+package lms2017a
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
---
+++ grails-app/init/lms2017a/BootStrap.groovy
@@ -0,0 +1,9 @@
+package lms2017a
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
